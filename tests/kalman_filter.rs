312f3473dea0222c76671edbdb45c31cdde266c9
Guilherme Dytz <guidytz@gmail.com>
2022-08-23 18:06:06 -0300

    Creating tests

---
+++ tests/kalman_filter.rs
@@ -0,0 +1,48 @@
+mod model;
+use std::fs;
+
+use kalman_filter::{
+    KalmanFilter, ACCELERATION_STD_DEFAULT, INITIAL_UNCERTAINTY_DEFAULT, POSITION_STD_DEFAULT,
+};
+use model::kalman_coordinate::KalmanCoordinates;
+
+fn kalman_filter(coordinates: KalmanCoordinates) {
+    let initial_uncertainty = INITIAL_UNCERTAINTY_DEFAULT;
+    let acc_std = ACCELERATION_STD_DEFAULT;
+    let position_std = POSITION_STD_DEFAULT;
+
+    let mut last_time = coordinates[0].received_time;
+    let mut kalman_filter = KalmanFilter::builder(
+        geo::Coordinate {
+            x: coordinates[0].lon as f32,
+            y: coordinates[0].lat as f32,
+        },
+        initial_uncertainty,
+    )
+    .uncertainty_increase_multiplier(10000f32)
+    .build();
+
+    let mut count = 0;
+    for coordinate in coordinates[1..].into_iter() {
+        count += 1;
+        let elapsed_time = coordinate.received_time - last_time;
+        last_time = coordinate.received_time;
+        kalman_filter.predict(elapsed_time.num_seconds() as f32, acc_std);
+        print!("{count} - {:#} - ", coordinate.received_time);
+
+        let coordinate = geo::Coordinate {
+            x: coordinate.lon as f32,
+            y: coordinate.lat as f32,
+        };
+        kalman_filter.update(coordinate, position_std);
+    }
+}
+
+#[test]
+fn read_and_test_file() {
+    let raw = fs::read_to_string("tests/data/lh-kalman-another-example.json")
+        .expect("Couldn't read file");
+
+    let coordinates: KalmanCoordinates = serde_json::from_str(&raw).expect("Couldn't parse json");
+    kalman_filter(coordinates);
+}
---
+++ tests/model/kalman_coordinate.rs
@@ -0,0 +1,64 @@
+use std::ops::{Deref, DerefMut};
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KalmanCoordinate {
+    pub received_time: DateTime<Utc>,
+    pub server_time: DateTime<Utc>,
+    pub collected_time: DateTime<Utc>,
+    pub tracker_id: i64,
+    pub accuracy: f64,
+    pub lat: f64,
+    pub lon: f64,
+    pub speed: f64,
+    pub event: String,
+    pub acc_status: Option<bool>,
+    pub battery: Option<i32>,
+    pub imei: Option<String>,
+    pub alarm_type: Option<i32>,
+    pub kalman_info: KalmanInfo,
+}
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KalmanInfo {
+    latitude: f64,
+    longitude: f64,
+    lat_speed: f32,
+    lon_speed: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KalmanCoordinates(pub Vec<KalmanCoordinate>);
+
+impl From<Vec<KalmanCoordinate>> for KalmanCoordinates {
+    fn from(data: Vec<KalmanCoordinate>) -> Self {
+        KalmanCoordinates(data)
+    }
+}
+
+impl IntoIterator for KalmanCoordinates {
+    type Item = KalmanCoordinate;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl Deref for KalmanCoordinates {
+    type Target = Vec<KalmanCoordinate>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for KalmanCoordinates {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
